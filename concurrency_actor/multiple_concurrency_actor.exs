90d3e684ea75d72dccf56f8a5eed8ac82ccb8aa0
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-08-31 11:15:00 +0100

    better output

---
+++ concurrency_actor/multiple_concurrency_actor.exs
@@ -0,0 +1,21 @@
+defmodule SpawnMultiple do
+    def greet do
+        receive do
+            {callerPid, msg} ->
+                send callerPid, {:ok, "IM THE PROCESS!!!, HELLO #{msg}"}
+        end
+    end
+end
+
+# the client send to the process..
+pidProcess = spawn(SpawnMultiple, :greet, [])
+send pidProcess, {self, "world!!!!"}
+
+IO.inspect pidProcess
+IO.inspect self
+
+# and the client receive response from the process...
+receive do
+    {:ok, message} ->
+        IO.puts message # IM THE PROCESS!!!, HELLO world!!!!
+end
---
+++ concurrency_actor/simple_concurrency_actor.exs
@@ -0,0 +1,22 @@
+defmodule SpawnBasic do
+    def greet do
+        receive do
+            {callerPid, msg} ->
+                send callerPid, {:ok, "IM THE PROCESS!!!, HELLO #{msg}, I see your PID is:" <> inspect(callerPid)}
+        end
+    end
+end
+
+# the client send to the process..
+pidProcess = spawn(SpawnBasic, :greet, [])
+send pidProcess, {self, "world!!!!"}
+
+IO.puts "client process: " <> inspect(self)
+IO.puts "spawned process: " <> inspect(pidProcess)
+
+
+# and the client receive response from the process...
+receive do
+    {:ok, message} ->
+        IO.puts message # IM THE PROCESS!!!, HELLO world!!!!
+end
