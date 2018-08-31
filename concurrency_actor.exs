ef85193ccff8338218eca597ffc394b40997a2ad
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-08-31 11:08:10 +0100

    first steps in actor pattern concurrency

--- concurrency_actor.exs
+++ concurrency_actor.exs
@@ -1,5 +1,21 @@
 defmodule SpawnBasic do
     def greet do
-        IO.puts "Hello"
+        receive do
+            {callerPid, msg} ->
+                send callerPid, {:ok, "IM THE PROCESS!!!, HELLO #{msg}"}
+        end
     end
+end
+
+# the client send to the process..
+pidProcess = spawn(SpawnBasic, :greet, [])
+send pidProcess, {self, "world!!!!"}
+
+IO.inspect pidProcess
+IO.inspect self
+
+# and the client receive response from the process...
+receive do 
+    {:ok, message} ->
+        IO.puts message # IM THE PROCESS!!!, HELLO world!!!!
 end
---
+++ modules.exs
@@ -0,0 +1,7 @@
+defmodule SpawnBasic do
+    def greet do
+        IO.puts "Hello"
+    end
+end
+
+SpawnBasic.greet
