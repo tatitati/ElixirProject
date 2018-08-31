219b8b6e27173934d95fe3bf642cfa0d303f0b1b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-08-31 16:03:50 +0100

    more

--- strings.exs
+++ strings.exs
@@ -1,7 +1,11 @@
-# concat lists
-a = [2, 3]
-b = a ++ [4, 5]
-IO.inspect(b) # [2, 3, 4, 5]
+# immutable data????? : 
+# in Elixir you can rebind variables (change the meaning of the "label"), mainly for your convenience:
+# ......so......YOU CANNOT MODIFY IT, BUT YOU CAN RE-BIND A VARIABLE (change the whole reference of a variable). Well, like Value objects, you cannot modify them, 
+# but you can always overwrite your varaible with another value object
+a = "somethingA "
+a = a <> "somethinbB"
+IO.inspect(a) # "somethingA somethingB"
+
 
 
 # concat strings
@@ -21,3 +25,5 @@ IO.puts "anyname " <> "OTHER TEXT" #anyname OTHER TEXT
 # so we can know what that "credentials" value is
 "Authentication: " <> credentials = "Authentication: whatever text you want"
 IO.puts credentials # whatever text you want
+
+
