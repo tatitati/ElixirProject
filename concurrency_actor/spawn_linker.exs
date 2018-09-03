ef8a7d6a34c3d98e9a4ecb0a29322f72c13f26e4
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-03 23:22:01 +0100

    more

--- concurrency_actor/spawn_linker.exs
+++ concurrency_actor/spawn_linker.exs
@@ -1,4 +1,4 @@
-defmodule SuiciderProcess do
+defmodule SpawnLinker do
     import :timer, only: [sleep: 1]
 
     def sad_function do
@@ -12,7 +12,7 @@ defmodule SuiciderProcess do
 
         # If we spawn as usual, then we will get only "NOTHING HAPPEND", our client won't be notified about the death of our sad_function. 
         # However, if we spawn_link, then we will get a notification
-        res = spawn_link(SuiciderProcess, :sad_function, [])
+        res = spawn_link(SpawnLinker, :sad_function, [])
         IO.puts inspect res # {#PID<0.94.0>, #Reference<0.1574448092.97779713.121224>}
         receive do
             msg ->
@@ -24,4 +24,4 @@ defmodule SuiciderProcess do
 end
 
 # IMPORTANT: THIS KILL THE PROCESS, NO MESSAGE LIKE "NOTHING HAPPEND, OR MESSAGE RECEIVED", spawn_link kill all linked process when one of the linked one die
-SuiciderProcess.run # ** (EXIT from #PID<0.89.0>) :boom      
+SpawnLinker.run # ** (EXIT from #PID<0.89.0>) :boom
