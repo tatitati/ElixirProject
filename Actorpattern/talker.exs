5cc52825110af2db2d9152833f7b597f970f9a1a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-03 22:22:46 +0100

    basic actor

---
+++ Actorpattern/talker.exs
@@ -0,0 +1,20 @@
+defmodule Talker do
+    def loop do 
+        receive do 
+            {:greet, name} -> IO.puts("Hello #{name}!!!")
+            {:bye, name} -> IO.puts("Bye #{name}!!!")
+            {:congrats, name} -> IO.puts("Congrats #{name}!!!")
+        end
+        loop
+    end
+end
+
+
+## run actor server
+
+pid = spawn(Talker, :loop, [])
+
+## execute client
+send(pid, {:greet, "Francisco"} # Hello Francisco!!!
+
+
