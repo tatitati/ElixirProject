3d68ec2bb34dead04b04e06f0bf751a7ce28d154
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-05 00:17:57 +0100

    researching exit codes

--- concurrency_actor/spawn_linker.exs
+++ concurrency_actor/spawn_linker.exs
@@ -8,15 +8,21 @@ defmodule SpawnLinker do
 
     def run do
         # if we uncomment this line we will get "MESSAGE RECEIVED: EXIT FROM PID......". The output of the process will be converted to something that the client can handle (a msg)
-        #Process.flag(:trap_exit, true)
+        Process.flag(:trap_exit, true)
 
-        # If we spawn as usual, then we will get only "NOTHING HAPPEND", our client won't be notified about the death of our sad_function. 
+        # If we spawn as usual, then we will get only "NOTHING HAPPEND", our client won't be notified about the death of our sad_function.
         # However, if we spawn_link, then we will get a notification
         res = spawn_link(SpawnLinker, :sad_function, [])
         IO.puts inspect res # {#PID<0.94.0>, #Reference<0.1574448092.97779713.121224>}
         receive do
+            # I can specify in these two ways the pattern matching.
+            # 1. I can specify all the fields of the exit message.
+            # 2. Or I can say that is just any trapped message (laster, inspecting, I see all the fields).
+            # I can comment any of these two options and see the corresponding outputs
+            {:EXIT, pid, reason} ->
+                IO.puts reason # boom
             msg ->
-                IO.puts "MESSAGE RECEIVED: #{inspect msg}"
+                IO.puts "MESSAGE RECEIVED: #{inspect msg}" # MESSAGE RECEIVED: {:EXIT, #PID<0.94.0>, :boom}
             after 1000 ->
                 IO.puts "NOTHING HAPPEND"
         end
@@ -24,4 +30,4 @@ defmodule SpawnLinker do
 end
 
 # IMPORTANT: THIS KILL THE PROCESS, NO MESSAGE LIKE "NOTHING HAPPEND, OR MESSAGE RECEIVED", spawn_link kill all linked process when one of the linked one die
-SpawnLinker.run # ** (EXIT from #PID<0.89.0>) :boom
+SpawnLinker.run
