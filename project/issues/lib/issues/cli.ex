a7e9b2b61ecb35d52ff0738fb3d58c78d3df84e0
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-05 23:27:14 +0100

    More tests

--- project/issues/lib/issues/cli.ex
+++ project/issues/lib/issues/cli.ex
@@ -1,5 +1,16 @@
 defmodule Issues.Cli do
     def parse_args(argv) do
-        :help
+        parse = OptionParser.parse!(argv,
+            switches: [help: :boolean],
+            aliases: [h: :help]
+        )
+
+        case parse do
+            # parse =  {[help: true], ["anything"]}
+            {[help: true], _ } -> :help
+
+            # parse = {[], ["One", "Two", "Three"]}
+            {_ , [arg1, arg2, arg3]} -> {arg1, arg2, arg3}
+        end
     end
 end
--- project/issues/test/cli_test.exs
+++ project/issues/test/cli_test.exs
@@ -1,8 +1,14 @@
 defmodule CliTest do
     use ExUnit.Case
+    # if this module is in a exs module then the import won't work. Elixir is compiled
+    import Issues.Cli
 
-    test "My first test" do
-        assert Issues.Cli.parse_args(["-h", "anything"]) == :help
+    test "Alias for help option work" do
+        assert parse_args(["-h", "anything"]) == :help
+        assert parse_args(["-help", "anything"]) == :help
     end
 
+    test "Parser can split all arguments" do
+        assert parse_args(["One", "Two", "Three"]) == {"One", "Two", "Three"}
+    end
 end
