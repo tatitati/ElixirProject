8cf343b3c1a856c5081609b1731f690324847541
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-09 23:52:14 +0100

    some progress

--- project/issues/lib/issues/github_issues.ex
+++ project/issues/lib/issues/github_issues.ex
@@ -1,6 +1,6 @@
 defmodule Issues.GithubIssues do
     @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
-
+    @github_url Application.get_env(:issues, :github_url)
     #example use:
     # Issues.GithubIssues.fetch("elixir-lang", "elixir")
 
@@ -11,10 +11,10 @@ defmodule Issues.GithubIssues do
     end
 
     def issues_url(user, project) do
-        "https://api.github.com/repos/#{user}/#{project}/issues"
+        "#{@github_url}/repos/#{user}/#{project}/issues"
     end
 
     def handle_response(%{status_code: 200, body: body}),  do: {:ok, body}
 
-    def handle_response(%{status_code: ___, body: body}),  do: {:error, body}
+    def handle_response(%{status_code: ___, body: body}),  do: {:error, :jsx.decode(body)}
 end
