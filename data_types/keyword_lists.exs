79757f40356aded23af40edebfd25d8a79e1aca7
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-30 20:36:36 +0100

    more

--- data_types/keyword_lists.exs
+++ data_types/keyword_lists.exs
@@ -1,3 +1,8 @@
 
 keywordlist = [a: "something", b: "whatever"]
 IO.inspect(keywordlist) # [a: "something", b: "whatever"]
+
+
+[something, something_else] = [a: "something", b: "whatever"]
+IO.inspect something # {:a, "something"}
+IO.inspect something_else # {:b, "whatever"}
