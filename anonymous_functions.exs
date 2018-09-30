93cb18b8d92388181d4d76a256a5dd42b8771758
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-30 01:55:44 +0100

    moer

--- anonymous_functions.exs
+++ anonymous_functions.exs
@@ -1,16 +1,20 @@
 #######
 myfirstfunction = fn name -> IO.puts("hello " <> name) end
+myfirstfunction.("maria") # hello maria
 
+
+#######
 myfirstfunctionMultiline = fn name -> 
     IO.puts("hello " <> name) 
 end
-
-myfirstfunction.("maria") # hello maria
 myfirstfunctionMultiline.("eva") # hello eva
 
 
 #######
-ivaLambda = fn orderPrice -> orderPrice * 2 / 100 end
 priceCalculatorLambda = fn orderPrice, ivaCalculator -> orderPrice + ivaCalculator.(orderPrice) end
 
+ivaLambda = fn orderPrice -> orderPrice * 2 / 100 end
 IO.puts(priceCalculatorLambda.(34, ivaLambda)) # 34.68
+
+
+# using shortcuts (capturing arguments)
